8b9e3e9a14cc964c5c0d6a022fddd588ef906f3a
iZeaoGamer <jameshayward14@icloud.com>
2017-06-25 16:36:37 +0100

    Updated Vanish.php

--- src/erkam2002/Vanish.php
+++ src/erkam2002/Vanish.php
@@ -25,9 +25,9 @@ class Vanish extends PluginBase implements Listener {
         $this->saveResource("config.yml");
         @mkdir($this->getDataFolder());
         $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, [
-            "Creative_Vanish" => true
+            "Creative_Vanish" => false
         ]);
-        $this->config->set("Creative_Vanish", true);
+        $this->config->set("Creative_Vanish", false);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
@@ -39,20 +39,20 @@ class Vanish extends PluginBase implements Listener {
                     $this->vanish[] = $name;
                     $sender->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_INVISIBLE, true);
                     $sender->setNameTagVisible(false);
-                    if($this->config->get("Creative_Vanish") == true){
+                    if($this->config->get("Creative_Vanish") == false){
                         $sender->setGamemode(1);
                     }
-                    $sender->sendMessage($this->prefix . C::GREEN . "You are now vanished.");
+                    $sender->sendMessage($this->prefix . C::GREEN . "You are now super vanished. No one can see you.");
                 } elseif (in_array($name, $this->vanish)) {
                     unset($this->vanish[array_search($name, $this->vanish)]);
                     $sender->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_INVISIBLE, false);
                     $sender->setNameTagVisible(true);
-                    if($this->config->get("Creative_Vanish") == true){
+                    if($this->config->get("Creative_Vanish") == false)
                         $sender->setGamemode(0);
                     }
                     $sender->setHealth(20);
                     $sender->setFood(20);
-                    $sender->sendMessage($this->prefix . C::RED . "You are no longer vanished!");
+                    $sender->sendMessage($this->prefix . C::RED . "You are no longer super vanished! everyone should be able to see you.");
                 }
             }
         }
