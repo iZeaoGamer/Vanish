9b6f277740d2085f2c818335a379cdac0a674228
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-04 20:17:35 +0100

    Update Vanish.php

--- src/ErkamKahriman/Vanish/Vanish.php
+++ src/ErkamKahriman/Vanish/Vanish.php
@@ -24,12 +24,14 @@ class Vanish extends PluginBase implements Listener {
                     if ($this->vanish[$name] == false) {
                         $this->vanish[$name] = true;
                         $sender->sendMessage(self::PREFIX . C::GREEN . "§dYou are now vanished. §5No one can see you.");
+                        $sender->sendMessage(self::PREFIX . C::GREEn . "§c$name §ehas left the game.");
                     } else {
                         $this->vanish[$name] = false;
                         foreach ($this->getServer()->getOnlinePlayers() as $players){
                             $players->showPlayer($sender);
                         }
                         $sender->sendMessage(self::PREFIX . C::RED . "§dYou are no longer vanished! §bEveryone can now see you!");
+                        $sender->sendMessage(self::PREFIX . C::RED . "§a$name §chas joined the game");
                     }
                 }
             } else {
