8e0152c09fa140b2cea300e432d0f02742336d48
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-06 12:03:53 +0100

    Fixed Titles.

--- src/ErkamKahriman/Vanish/Vanish.php
+++ src/ErkamKahriman/Vanish/Vanish.php
@@ -24,7 +24,7 @@ class Vanish extends PluginBase implements Listener {
                         $this->vanish[$name] = true;
                         $sender->sendMessage(self::PREFIX . C::GREEN . " §dYou are now vanished. §5No one can see you.");
                         $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), (99999999*20), (1), (false)));
-                        $sender->getPlayer()->addTitle("§6§lVanish Mode is §5§lenabled!", 40, 100, 40);
+                        $event->getPlayer()->addTitle("§6§lVanish Mode", "is §5§lenabled!", 40, 100, 40);
                         $this->getServer()->broadcastMessage(C::GREEN . "§c$name §ehas left the game.");
                     } else {
                         $this->vanish[$name] = false;
@@ -33,7 +33,7 @@ class Vanish extends PluginBase implements Listener {
                         }
                         $sender->sendMessage(self::PREFIX . C::RED . " §dYou are no longer vanished! §bEveryone can now see you!");
                         $sender->removeEffect(Effect::NIGHT_VISION);
-                        $sender->getPlayer()->addTitle("§6§lVanish mode is §c§lDisabled", 40, 100, 40);
+                        $event->getPlayer()->addTitle("§6§lVanish mode", "is §c§lDisabled", 40, 100, 40);
                         $this->getServer()->broadcastMessage(C::RED . "§a$name §ehas joined the game");
                     }
                 }
